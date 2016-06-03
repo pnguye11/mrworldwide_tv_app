5b07342a6c498196bc86956b17ab1f5ab220ba8b
Phuong Nathan Nguyen <pnguye11@gmail.com>
2016-06-03 11:40:58 -0700

    user model updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,22 @@
 User.delete_all
 
 # user = User.new
-# user_password = "123"
-# user_password_confirmation = '123'
+# user.password = "1234"
+# user.password_confirmation = "1234"
 
 pj = User.new(
-  email: "pj@ga.co",
-  password: "123",
-  password_confirmation:"123",
+  email:                 "pj@ga.co",
+  password:              "1234",
+  password_confirmation: "1234",
 )
 pj.save
 
-puts user.errors.full_messages
+ez = User.new(
+  email:                 "ez@ga.co",
+  password:              "1234",
+  password_confirmation: "1234",
+  handle:                "ezrazilla",
+  profile_url:           "https://ga-core.s3.amazonaws.com/production/uploads/instructor/image/6752/thumb_harsh_ez.jpg",
+  statement:             "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defense, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America."
+)
+ez.save
